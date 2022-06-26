0a6b57a140baf442df466ead3d5db9bf6d562431
HeonchanKim <khc9274@naver.com>
2022-06-26 13:29:35 +0900

    controller 실습

--- workspace/ex02/src/main/java/com/example/ex02/controller/SampleController.java
+++ workspace/ex02/src/main/java/com/example/ex02/controller/SampleController.java
@@ -5,12 +5,15 @@ import java.util.ArrayList;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.ex02.domain.vo.InfoDTO;
+import com.example.ex02.domain.vo.StudentVO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -52,6 +55,60 @@ public class SampleController {
 		log.info("datas: " + datas);
 		return "ex03";
 	}
+	
+	@GetMapping("/ex04")
+	//만약 매개변수가 객체라면, 해당 클래스타입의 앞글자만 소문자로 변경된 값이
+	//화면에서 사용할 key값이다.
+	//예) 매개변수의 타입이 InfoDTO infoDTO가 매개변수라면, 화면에서 사용 시 key값은 infoDTO가 된다.
+	//만약에 key값을 수정하거나 매개변수가 많아진다면,
+	//직접 requestScope에 담아서 전달해야 한다.
+	//이 때 request객체를 직접 불러오지 않고, Model이라는 데이터 전달자를 사요하게 된다.
+	//하지만 화면쪽에 전달할 데이터가 여러 개가 아니라면, @ModelAttribute()를 사용하여 화면에 전달해준다.
+	//@ModelAtrribute("화면에서 사용할 KEY")
+	public String ex04(@ModelAttribute("dto") InfoDTO infoDTO) {
+		log.info("--------------------------");
+		log.info("ex04......................");
+		log.info(infoDTO.toString());
+		log.info("--------------------------");
+		
+		return "ex04";
+	}
+	
+	@GetMapping("/ex05")
+	public void ex05(InfoDTO infoDTO, @ModelAttribute("gender") String gender) {
+		log.info("ex05........");
+		log.info(infoDTO.toString());
+		log.info("gender : " + gender);
+	}
+	
+	@GetMapping("/ex06")
+	//Model 객체는 파라미터로 request객체를 받는다.
+	//따라서 여러 개의 데이터를 화면에 전달할 때
+	//addAttribute(KEY, VALUE)을 사용한다.
+	//화면에서 model에 설정한 KEY로 VALUE를 사용할 수 있다.
+	public String ex06(InfoDTO infoDTO, String gender, Model model) {
+		log.info("ex05........");
+		log.info(infoDTO.toString());
+		log.info("gender : " + gender);
+		
+		model.addAttribute("dto", infoDTO);
+		model.addAttribute("gender", gender);
+		return "ex/ex06";
+	}
+	
+	@GetMapping("/ex07")
+	//외부에서 학생의 번호, 국어, 영어, 수학 점수를 모델 객체로 전달받는다.
+	//파라미터명과 매개변수에 선언된 모델객체의 필드명이 동일ㄹ하면 자동을 매핑된다.
+	//리턴 시 알맞는 페이지로 이동이 되고, 모델 객체는 직접 KEY를 지정하지 않아도
+	//화면쪽에서는 앞글자만 소문자로 바뀐 값으로 해당 필드를 접근할 수 있다.
+	public String ex07(StudentVO studentVO) {
+		log.info("ex07...........");
+		log.info(studentVO.toString());
+		
+		return "ex/ex07";
+	}
+	
+	
 	
 	
 	
