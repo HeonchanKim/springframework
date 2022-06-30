be53b8b3c08f6662bdb01b5ff86f1576a6db2aa3
HeonchanKim <khc9274@naver.com>
2022-06-30 20:22:32 +0900

    404, 500 Error 처리

--- workspace/ex02/src/main/java/com/example/ex02/controller/SampleController.java
+++ workspace/ex02/src/main/java/com/example/ex02/controller/SampleController.java
@@ -4,13 +4,17 @@ import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.example.ex02.domain.vo.InfoDTO;
 import com.example.ex02.domain.vo.StudentVO;
@@ -108,7 +112,11 @@ public class SampleController {
 		return "ex/ex07";
 	}
 	
-	
+	@ExceptionHandler(NoHandlerFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public String except404(NoHandlerFoundException e) {
+		return "error/404";
+	}
 	
 	
 	
